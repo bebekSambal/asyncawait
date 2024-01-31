8c7e01579f658a0224fa1cb898a3ba0b47a79ed3
bebek <155866565+bebekSambal@users.noreply.github.com>
2024-01-31 20:40:54 +0700

    Update app-1.js
    belajar async wait dengan callback dan dengan promise

--- promise/app-1.js
+++ promise/app-1.js
@@ -1,16 +1,40 @@
-const rekuest = (link, sukses, gagal)=>{
-    const delay = Math.floor(Math.random()* 4500) + 300;
-    setTimeout(()=>{
-        if (delay > 2700){
-            gagal(`gagal: koneksi habizzz ${delay}ms` );
-        } else {
-            sukses(`selamat! ${link} ${delay}`);
-        }
-    },delay);
+// versi callback
+// const rekuest = (link, sukses, gagal)=>{
+//     const delay = Math.floor(Math.random()* 4500) + 300;
+//     setTimeout(()=>{
+//         if (delay > 2700){
+//             gagal(`gagal: koneksi habizzz ${delay}ms` );
+//         } else {
+//             sukses(`selamat! ${link} ${delay}`);
+//         }
+//     },delay);
+// };
+
+// rekuest('jancok.com', function(response){
+//     console.log('YES!', response);
+// }, function(error){
+//     console.log('RUNGKAD rek', error);
+// });
+
+
+// versi promise
+const requestPromise = (url) =>{
+    return new Promise((resolve, reject) =>{
+        const delay = Math.floor(Math.random() *4300) +533;
+        setTimeout(()=>{
+            if (delay > 3700) {
+                reject('error: koneksi pedhot su');
+            } else {
+                resolve(`sukses bos : ${url} ${delay}ms`);
+            };
+        },delay);
+    });
 };
 
-rekuest('jancok.com', function(response){
-    console.log('YES!', response);
-}, function(error){
-    console.log('RUNGKAD rek', error);
-});
+requestPromise('google.com')
+    .then((response) => {
+        console.log('succes', response);
+    })
+    .catch((error) => {
+        console.log('error', error);
+    });
